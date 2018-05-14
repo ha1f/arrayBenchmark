90196ca0574c91e48710986835427c30801d9c5f
ha1f <half.ldk.hiz@gmail.com>
2018-05-15 00:39:53 +0900

    Initial Commit

---
+++ arrayBenchmarkMac/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  arrayBenchmarkMac
+//
+//  Created by はるふ on 2018/05/15.
+//  Copyright © 2018年 はるふ. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

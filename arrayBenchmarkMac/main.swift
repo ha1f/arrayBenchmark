a9d1fd74d3627fc6a9bdf0e97882dec31388fdd7
ha1f <half.ldk.hiz@gmail.com>
2018-05-15 01:40:02 +0900

    update

--- arrayBenchmarkMac/main.swift
+++ arrayBenchmarkMac/main.swift
@@ -11,11 +11,7 @@ import Foundation
 var array: [Int] = []
 var previous = Date()
 for _ in 0..<1048580 {
-//for _ in 0..<10000000 {
     array.append(0)
-//    array.withUnsafeBufferPointer { pointer in
-//        print(pointer.baseAddress)
-//    }
     let now = Date()
     print(now.timeIntervalSince(previous))
     previous = now

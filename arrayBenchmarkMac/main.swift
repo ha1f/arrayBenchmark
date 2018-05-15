ab2981dda313b76dd7bb4ce4e90a4e64e6eec948
ha1f <half.ldk.hiz@gmail.com>
2018-05-16 01:04:53 +0900

    add

--- arrayBenchmarkMac/main.swift
+++ arrayBenchmarkMac/main.swift
@@ -8,6 +8,27 @@
 
 import Foundation
 
+extension Array {
+    func printAddress() {
+        self.withUnsafeBufferPointer { pointer in
+            print(pointer.baseAddress)
+        }
+    }
+    
+    func showWithPointer(count: Int) {
+        self.withUnsafeBufferPointer { pointer in
+            let seed = pointer.baseAddress?.advanced(by: count)
+            print(seed, seed?.pointee)
+        }
+    }
+    
+    func showLastPointer() {
+        self.withUnsafeBufferPointer { pointer in
+            print(pointer.baseAddress?.advanced(by: pointer.endIndex).predecessor())
+        }
+    }
+}
+
 func printIntervals() {
     var array: [Int] = []
     var previous = Date()
@@ -21,16 +42,52 @@ func printIntervals() {
 
 func printPoints() {
     var array: [Int] = []
-    var previousAddress: UnsafeMutablePointer<Int>?
+    var previousAddress: UnsafePointer<Int>?
     for i in 0..<10000000 {
         array.append(0)
-        array.withUnsafeMutableBufferPointer { pointer in
+        array.withUnsafeBufferPointer { pointer in
             if pointer.baseAddress != previousAddress {
-                print(i, pointer.baseAddress!)
+                print(i, pointer.baseAddress!, array.capacity)
                 previousAddress = pointer.baseAddress
             }
         }
     }
 }
 
+func replaceLetArray() {
+    let array: [Int] = (0..<100).map { $0 }
+    let pointer = UnsafeMutablePointer<Int>(mutating: array)
+    
+    print(pointer.advanced(by: 50).pointee)
+    
+    pointer.assign(repeating: 1, count: 80)
+    
+    print(pointer.advanced(by: 50).pointee)
+}
+
+
+func removeArray() {
+    var array: [Int] = (0..<100).map { $0 }
+    
+    array.printAddress()
+    array.showWithPointer(count: 50)
+    array.showLastPointer()
+    print()
+    
+    
+    array.remove(at: 50)
+    array.printAddress()
+    array.showWithPointer(count: 50)
+    array.showLastPointer()
+    print()
+    
+    array.remove(at: 20)
+    array.printAddress()
+    array.showWithPointer(count: 50)
+    array.showLastPointer()
+}
+
+// removeArray()
 printPoints()
+
+
